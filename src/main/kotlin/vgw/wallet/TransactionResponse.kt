9781c4ad0ef9b87e277bd2eef70ec3ebd334d49e
Jake Gaske <jake.Gaske@vgw.co>
2023-05-22 14:26:33 +0800

    Cleanup

--- src/main/kotlin/vgw/wallet/TransactionResponse.kt
+++ src/main/kotlin/vgw/wallet/TransactionResponse.kt
@@ -1,16 +1,5 @@
 package vgw.wallet
 
-enum class WalletResponse {
-    DuplicateTransaction,
-    Created,
-    InputError,
-    Ok,
-    NotFound,
-}
-
-
-data class BalanceWalletResponse(var response: WalletResponse, var balance: Balance)
-
 sealed interface QueryResponse {
     data class Success(val wallet: Wallet) : QueryResponse
     data class WalletNotFound(val msg: String = "Wallet Not Found") : QueryResponse
